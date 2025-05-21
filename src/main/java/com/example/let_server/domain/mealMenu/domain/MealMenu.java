dba1a225cc03da572107720b78e92e626da803a2
lyhtll <a62346986@gmail.com>
2025-05-21 20:13:38 +0900

    feat :: MealMenu Entity

---
+++ src/main/java/com/example/let_server/domain/mealMenu/domain/MealMenu.java
@@ -0,0 +1,10 @@
+package com.example.let_server.domain.mealMenu.domain;
+
+import com.example.let_server.domain.meal.domain.Meal;
+import com.example.let_server.domain.menu.domain.Menu;
+
+public class MealMenu {
+    private Long mealMenuId;
+    private Menu menu;
+    private Meal meal;
+}

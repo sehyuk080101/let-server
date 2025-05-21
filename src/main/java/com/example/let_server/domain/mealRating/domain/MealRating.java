e40835032b5d163f92a9d57ad7be11f5418b4f16
lyhtll <a62346986@gmail.com>
2025-05-21 20:13:46 +0900

    feat :: MealRating Entity

---
+++ src/main/java/com/example/let_server/domain/mealRating/domain/MealRating.java
@@ -0,0 +1,9 @@
+package com.example.let_server.domain.mealRating.domain;
+
+import com.example.let_server.domain.meal.domain.Meal;
+
+public class MealRating {
+    private Long mealRatingId;
+    private Meal meal;
+    private float rating;
+}

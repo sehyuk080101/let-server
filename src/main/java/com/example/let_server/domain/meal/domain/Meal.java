8aa2cb9ec51832fe72c47a9cab3fed3e7008e6fc
lyhtll <a62346986@gmail.com>
2025-05-21 20:13:24 +0900

    feat :: Meal

---
+++ src/main/java/com/example/let_server/domain/meal/domain/Meal.java
@@ -0,0 +1,12 @@
+package com.example.let_server.domain.meal.domain;
+
+import java.util.Date;
+
+public class Meal {
+    private int mealId;
+    private Date mealTime;
+    private Long menuScore;
+    private Long likeCount;
+    private Long dislikeCount;
+    private int currentRank;
+}

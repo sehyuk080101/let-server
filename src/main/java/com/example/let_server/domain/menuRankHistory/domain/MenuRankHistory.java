df54a3968fc2dd437ac0b02a54bacc5bf515f35a
lyhtll <a62346986@gmail.com>
2025-05-21 20:14:12 +0900

    feat :: MenuRankHistory Entity

---
+++ src/main/java/com/example/let_server/domain/menuRankHistory/domain/MenuRankHistory.java
@@ -0,0 +1,13 @@
+package com.example.let_server.domain.menuRankHistory.domain;
+
+import com.example.let_server.domain.menu.domain.Menu;
+
+import java.util.Date;
+
+public class MenuRankHistory {
+    private Long historyId;
+    private Menu menu;
+    private Date date;
+    private int rank;
+    private int rankDiff;
+}

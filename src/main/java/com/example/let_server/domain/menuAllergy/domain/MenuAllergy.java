88ef9c2c3c672bf727440977d21ac91151b3029e
lyhtll <a62346986@gmail.com>
2025-05-21 20:14:02 +0900

    feat :: MenuAllergy Entity

---
+++ src/main/java/com/example/let_server/domain/menuAllergy/domain/MenuAllergy.java
@@ -0,0 +1,10 @@
+package com.example.let_server.domain.menuAllergy.domain;
+
+import com.example.let_server.domain.allergy.domain.Allergy;
+import com.example.let_server.domain.menu.domain.Menu;
+
+public class MenuAllergy {
+    private Long menuAllergyId;
+    private Allergy allergy;
+    private Menu menu;
+}

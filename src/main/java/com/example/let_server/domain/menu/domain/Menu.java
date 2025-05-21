ffc88a3879201a49c79da67bcb034c1214ddc45a
lyhtll <a62346986@gmail.com>
2025-05-21 20:13:52 +0900

    feat :: Menu Entity

---
+++ src/main/java/com/example/let_server/domain/menu/domain/Menu.java
@@ -0,0 +1,10 @@
+package com.example.let_server.domain.menu.domain;
+
+public class Menu {
+    private Long menuId;
+    private String menuName;
+    private double menuScore;
+    private Long likeCount;
+    private Long dislikeCount;
+    private int currentRank;
+}

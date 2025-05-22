125dc7504586bd99311124ad83193628ee91fb5d
lyhtll <a62346986@gmail.com>
2025-05-22 14:13:38 +0900

    feat :: mealMenu entity add annotation

--- src/main/java/com/example/let_server/domain/mealMenu/domain/MealMenu.java
+++ src/main/java/com/example/let_server/domain/mealMenu/domain/MealMenu.java
@@ -2,7 +2,15 @@ package com.example.let_server.domain.mealMenu.domain;
 
 import com.example.let_server.domain.meal.domain.Meal;
 import com.example.let_server.domain.menu.domain.Menu;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
 public class MealMenu {
     private Long mealMenuId;
     private Menu menu;

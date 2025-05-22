08f2ff2e1ab2e464c55cb85e52e70055efcfa9dd
lyhtll <a62346986@gmail.com>
2025-05-22 14:20:30 +0900

    feat :: mealMenu repository

---
+++ src/main/java/com/example/let_server/domain/mealMenu/repository/MealMenuRepository.java
@@ -0,0 +1,7 @@
+package com.example.let_server.domain.mealMenu.repository;
+
+import com.example.let_server.domain.mealMenu.domain.MealMenu;
+
+public interface MealMenuRepository {
+    MealMenu save(MealMenu mealMenu);
+}

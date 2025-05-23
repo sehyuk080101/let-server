ad2bd74666f3d505a60fb528acfce512ad55b33f
lyhtll <a62346986@gmail.com>
2025-05-23 14:18:46 +0900

    refactor :: MealResponse add attribute

--- src/main/java/com/example/let_server/domain/meal/dto/response/MealResponse.java
+++ src/main/java/com/example/let_server/domain/meal/dto/response/MealResponse.java
@@ -2,12 +2,16 @@ package com.example.let_server.domain.meal.dto.response;
 
 import com.example.let_server.domain.meal.domain.Meal;
 import com.example.let_server.domain.meal.domain.MealType;
+import com.example.let_server.domain.menu.domain.Menu;
+import com.example.let_server.domain.menu.dto.MenuResponse;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Data
 @Builder
@@ -18,6 +22,7 @@ public class MealResponse {
     private Date mealDate;
     private MealType mealType;
     private float calorie;
+    private List<MenuResponse> menus;
 
     public static MealResponse of(Meal meal) {
         return MealResponse.builder()
@@ -25,6 +30,7 @@ public class MealResponse {
                 .mealDate(meal.getMealDate())
                 .mealType(meal.getMealType())
                 .calorie(meal.getCalorie())
+                .menus(new ArrayList<>())
                 .build();
     }
 }

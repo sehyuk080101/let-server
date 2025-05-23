65e514088a1a0fc63af70ad076ced556dd2c5695
lyhtll <a62346986@gmail.com>
2025-05-23 09:13:53 +0900

    feat :: MealMenuMapper interface about findMonthlyMealMenu

--- src/main/java/com/example/let_server/domain/mealMenu/mapper/MealMenuMapper.java
+++ src/main/java/com/example/let_server/domain/mealMenu/mapper/MealMenuMapper.java
@@ -2,8 +2,12 @@ package com.example.let_server.domain.mealMenu.mapper;
 
 import com.example.let_server.domain.mealMenu.domain.MealMenu;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 @Mapper
 public interface MealMenuMapper{
     void save(MealMenu mealMenu);
+    List<MealMenu> findMonthlyMealMenu(@Param("yearMonth") String yearMonth);
 }

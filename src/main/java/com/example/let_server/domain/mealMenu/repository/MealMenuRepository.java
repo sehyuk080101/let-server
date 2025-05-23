3aed93ff0eb206225baa05eaa143e98b068879cf
lyhtll <a62346986@gmail.com>
2025-05-23 09:12:39 +0900

    feat :: MealMenuRepository about findMonthlyMealMenu

--- src/main/java/com/example/let_server/domain/mealMenu/repository/MealMenuRepository.java
+++ src/main/java/com/example/let_server/domain/mealMenu/repository/MealMenuRepository.java
@@ -2,6 +2,9 @@ package com.example.let_server.domain.mealMenu.repository;
 
 import com.example.let_server.domain.mealMenu.domain.MealMenu;
 
+import java.util.List;
+
 public interface MealMenuRepository {
     MealMenu save(MealMenu mealMenu);
+    List<MealMenu> findMonthlyMealMenu(String yearMonth);
 }

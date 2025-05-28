1028a52c3dc5b55df0f457645f88cc265eb5e7be
lyhtll <a62346986@gmail.com>
2025-05-29 08:56:56 +0900

    feat: enable scheduling with @EnableScheduling

--- src/main/java/com/example/let_server/LetServerApplication.java
+++ src/main/java/com/example/let_server/LetServerApplication.java
@@ -3,9 +3,11 @@ package com.example.let_server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
+@EnableScheduling
 public class LetServerApplication {
 
     public static void main(String[] args) {

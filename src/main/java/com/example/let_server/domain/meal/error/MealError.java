0251b2567f3f263431cd5f0583de8ede7eefb4a9
lyhtll <a62346986@gmail.com>
2025-05-28 11:10:03 +0900

    feat :: MealError

---
+++ src/main/java/com/example/let_server/domain/meal/error/MealError.java
@@ -0,0 +1,19 @@
+package com.example.let_server.domain.meal.error;
+
+import com.example.let_server.global.error.CustomError;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+public enum MealError implements CustomError {
+    MELA_TYPE_NOT_FOUND(404, "Meal Type not found");
+
+    private final int status;
+    private final String message;
+
+    @Override
+    public String getCode() {
+        return name();
+    }
+}
